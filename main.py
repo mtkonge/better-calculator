5a9bfb434fefc46d8c82918cf0a7cf23ebbfb569
Mikkel <mtkongsted@gmail.com>
2022-10-19 15:03:03 +0200

    started on parser

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class TokenType(Enum):
     DIVIDE = 4
     LPAREN = 5
     RPAREN = 6
+    NEGATIVE = 7
 
 class Token:
     def __init__(self, tt: TokenType, value: str):
@@ -24,31 +25,43 @@ DIGITS = "0123456789"
 def tokenize(text: str) -> List[Token]:
     tokens: List[Token] = []
     i = 0
-    isNumber = False
+    willBeNegative = True
     while i < len(text):
         if text[i] in " \n\t\r":
             i += 1
         elif text[i] == "+":
             tokens.append(Token(TokenType.PLUS, text[i]))
             i += 1
+            willBeNegative = True
         elif text[i] == "-":
-            tokens.append(Token(TokenType.MINUS, text[i]))
+            if willBeNegative:
+                tokens.append(Token(TokenType.NEGATIVE, text[i]))
+            else:
+                tokens.append(Token(TokenType.MINUS, text[i]))
             i += 1
+            willBeNegative = True
+        
         elif text[i] == "*":
             tokens.append(Token(TokenType.MULTIPLY, text[i]))
             i += 1
+            willBeNegative = True
         elif text[i] == "/":
             tokens.append(Token(TokenType.DIVIDE, text[i]))
             i += 1
+            willBeNegative = True
         elif text[i] == "(":
             tokens.append(Token(TokenType.LPAREN, text[i]))
             i += 1
+            willBeNegative = False
+
         elif text[i] == ")":
             tokens.append(Token(TokenType.RPAREN, text[i]))
             i += 1
+            willBeNegative = True
         elif text[i] in DIGITS:
             value = text[i]
             i += 1
+            willBeNegative = False
             while i < len(text) and text[i] in DIGITS:
                 value += text[i]
                 i += 1
@@ -69,7 +82,6 @@ class Int(Expr):
     def __str__(self) -> str:
         return f"Int {{ value: {self.value} }}"
 
-
 class Parser:
     def __init__(self, tokens: List[Token]):
         self.tokens = tokens
@@ -79,7 +91,32 @@ class Parser:
         return self.parseVal()
     
     def parseAddOrSub(self) -> Expr:
+        pass    
+
+    def mulOrDiv(self) -> Expr:
         pass
+
+    def parseNeg(self) -> Expr:
+        if self.tokens[self.i].tt == TokenType:
+            pass
+
+    #fn parse_negation() -> Expr {
+    #if let current_token() = TokenType::Minus {
+    #    let value = parse_negation();
+    #    return Expr::Negate(value);
+    #} else {
+    #    return parse_grouping();
+    #}
+#}
+
+
+    def parseGrp(self) -> Expr:
+        if self.tokens[self.i].tt == TokenType.LPAREN:
+            value = parseNeg()
+            self.skip(TokenType.RPAREN)
+            return self.parseNeg()
+        else:
+            return parseVal();
 
     def parseVal(self) -> Expr:
         if self.tokens[self.i].tt == TokenType.NUMBER:
@@ -89,13 +126,11 @@ class Parser:
             return expr
         else:
             raise Exception(f"Expected value, got {self.tokens[self.i]}")
+
+    def skip(self, tokenType: TokenType):
+        if self.tokens[self.i].tt == tokenType:
+            i += 1
         
-
-    
-    
-
-
-
 def main():
     with open("test.txt") as f:
         text = f.read()
